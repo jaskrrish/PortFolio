b2e2aed9e90497f85abe9ad5141a99cb304c6b55
jaskrrish <jashehz@gmail.com>
2024-03-05 19:44:10 +0530

    About-Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     extend: {
       colors: {
         primary: "#050816",
+        // primary: "#121212",
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
