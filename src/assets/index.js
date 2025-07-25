6f3a3c8d194c63bf39cc3318d3b66bd8a8174f55
jaskrrish <jashehz@gmail.com>
2025-07-25 10:27:40 +0530

    Refactor code structure for improved readability and maintainability

--- src/assets/index.js
+++ src/assets/index.js
@@ -22,7 +22,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import JasResume from "./jaskrrishresume-main.pdf";
+import JasResume from "./jaskrrish_cv-07:25.pdf";
 import meta from "./company/meta.png";
 import framerMotion from "./tech/framer-motion.png";
 import zod from "./tech/zod.png";
